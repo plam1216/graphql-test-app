e29a8686f7e89d3dd1a8eaca8eb6e477ac875f94
Peter Lam <plam1216@gmail.com>
2023-02-06 14:18:29 -0500

    setup graphiql

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const express = require('express')
 // Destructure functions
 const { graphqlHTTP } = require('express-graphql')
 
+const schema = require('./schema/schema')
+
 const PORT = 4000
 const app = express()
 
 
 app.use('/graphql', graphqlHTTP({
-
+    schema,
+    graphiql: true
 }))
 
 app.listen(PORT, () => {
