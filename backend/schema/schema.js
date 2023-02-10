791083e8691744c310aa71f894166009bb546c03
Peter Lam <plam1216@gmail.com>
2023-02-10 18:01:15 -0500

    add Author Relation and GraphQLList

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -5,17 +5,21 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = graphql
 
 const books = [
     { name: 'Book1', genre: 'fiction', id: '1', authorId: '1' },
-    { name: 'Book2', genre: 'nonfiction', id: '2', authorId: '2' }
+    { name: 'Book2', genre: 'nonfiction', id: '2', authorId: '2' },
+    { name: 'Book3', genre: 'childrens', id: '3', authorId: '2' },
+    { name: 'Book4', genre: 'fantasy', id: '4', authorId: '3' }
 ]
 
 const authors = [
     { name: 'Author1', age: 44, id: '1' },
-    { name: 'Author2', age: 62, id: '2' }
+    { name: 'Author2', age: 62, id: '2' },
+    { name: 'Author3', age: 33, id: '3' }
 ]
 
 // define Object Type on graph
@@ -32,8 +36,8 @@ const BookType = new GraphQLObjectType({
 
             // retrive author whose id property equals parent.id (book.authorId);
             resolve(parent, args) {
-                console.log(parent)
-                return _.find(authors, {id: parent.authorId})
+                // console.log(parent)
+                return _.find(authors, { id: parent.authorId })
             }
         }
     })
@@ -44,7 +48,18 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        age: { type: GraphQLInt },
+
+        // Type Relation
+        books: {
+            type: new GraphQLList(BookType),
+
+            // return books where authorId equals parent.id
+            resolve(parent, args) {
+                return _.filter(books, {authorId: parent.id})
+            }
+        }
+
     })
 })
 
