fda53fec0935b66ccbe2f48a5f6eafa79a829be5
Peter Lam <plam1216@gmail.com>
2023-02-11 18:34:49 -0500

    Create mongoose schemas

---
+++ backend/models/author.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const authorSchema = new Schema({
+    name: {type: String},
+    age: {type: Number},
+})
+
+const Author = mongoose.model('Author', authorSchema)
+module.exports = Author
